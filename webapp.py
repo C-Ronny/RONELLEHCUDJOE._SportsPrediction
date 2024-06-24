c1ea8504f0a3389a1fb291b9716d1dfb2411a94d
C-Ronny <138708517+C-Ronny@users.noreply.github.com>
2024-06-24 20:08:50 +0000

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -3,6 +3,7 @@ import pandas as pd
 import joblib
 import io
 import requests
+import pickle
 
 # Define the expected feature names used during training
 expected_features = ['movement_reactions', 'entality_composure', 'passing', 
@@ -22,6 +23,9 @@ def main():
     # Load the trained model and scaler from GitHub repository
     model_url = 'https://raw.githubusercontent.com/C-Ronny/RONELLEHCUDJOE._SportsPrediction/main/model_file.pkl'
     scaler_url = 'https://raw.githubusercontent.com/C-Ronny/RONELLEHCUDJOE._SportsPrediction/main/scaler.pkl'
+
+    model_response = requests.get(model_url)
+    model = pickle.load(io.BytesIO(model_response.content))
 
     # Download the model and scaler from the URL
     model_response = requests.get(model_url)
