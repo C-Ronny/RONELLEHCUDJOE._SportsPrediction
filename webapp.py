355aa15b38745f92c9d44012af058561fbbb8792
C-Ronny <138708517+C-Ronny@users.noreply.github.com>
2024-06-24 23:09:12 +0000

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -9,6 +9,7 @@ from sklearn.preprocessing import StandardScaler
 model_path = 'model_file.pkl'
 model = joblib.load(model_path)
 
+
 # Load the scaler
 scaler_path = 'scaler.pkl'
 scaler = joblib.load(scaler_path)
@@ -20,24 +21,18 @@ expected_features = ['movement_reactions', 'entality_composure', 'passing',
                      'power_shot_power', 'age']
 
 def main():
-    st.title("FIFA Rating Predictor")
+    st.title("FIFA Player Rating Predictor")
     html_temp = """
     <div style="background:#025246 ;padding:10px">
-    <h2 style="color:white;text-align:center;">Predict a Players Rating</h2>
+    <h2 style="color:white;text-align:center;">Player Rating Predictor App </h2>
     </div>
     """
     st.markdown(html_temp, unsafe_allow_html=True)
 
-    # Create two columns for input fields
-    col1, col2 = st.columns(2)
-
     # Create input fields for each feature with labels
     inputs = {}
-    for i, feature in enumerate(expected_features):
-        if i < len(expected_features) // 2:
-            inputs[feature] = col1.number_input(f"Enter {feature.replace('_', ').capitalize()} : ", value=0.0)
-        else:
-            inputs[feature] = col2.number_input(f"Enter {feature.replace('_', ').capitalize()} : ", value=0.0)
+    for feature in expected_features:
+        inputs[feature] = st.number_input(f"Enter {feature.replace('_', ' ').capitalize()} : ", value=0.0)
 
     # Create a button to predict the rating
     if st.button("Predict Rating"):
