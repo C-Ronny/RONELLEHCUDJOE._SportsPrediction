8f863b4f119311f5c27be11cd347de7fef95a24a
C-Ronny <138708517+C-Ronny@users.noreply.github.com>
2024-06-24 20:12:34 +0000

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,13 +1,26 @@
 import streamlit as st
 import pandas as pd
-import joblib
-import io
 import requests
+import joblib
+import os
+import io
+
+# Load the trained model and scaler from GitHub repository
+model_url = 'https://raw.githubusercontent.com/C-Ronny/RONELLEHCUDJOE._SportsPrediction/main/model_file.pkl'
+scaler_url = 'https://raw.githubusercontent.com/C-Ronny/RONELLEHCUDJOE._SportsPrediction/main/scaler.pkl'
+
+# Download the model and scaler from the URL
+model_response = requests.get(model_url)
+scaler_response = requests.get(scaler_url)
+
+# Load the model and scaler from the response
+model = joblib.load(io.BytesIO(model_response.content))
+scaler = joblib.load(io.BytesIO(scaler_response.content))
 
 # Define the expected feature names used during training
-expected_features = ['movement_reactions', 'entality_composure', 'passing', 
+expected_features = ['movement_reactions', 'mentality_composure', 'passing', 
                      'dribbling', 'physic', 'attacking_short_passing', 
-                     'entality_vision', 'kill_long_passing', 'hooting', 
+                     'mentality_vision', 'skill_long_passing', 'shooting', 
                      'power_shot_power', 'age']
 
 def main():
@@ -18,18 +31,6 @@ def main():
     </div>
     """
     st.markdown(html_temp, unsafe_allow_html=True)
-
-    # Load the trained model and scaler from GitHub repository
-    model_url = 'https://raw.githubusercontent.com/C-Ronny/RONELLEHCUDJOE._SportsPrediction/main/model_file.pkl'
-    scaler_url = 'https://raw.githubusercontent.com/C-Ronny/RONELLEHCUDJOE._SportsPrediction/main/scaler.pkl'
-
-    # Download the model and scaler from the URL
-    model_response = requests.get(model_url)
-    scaler_response = requests.get(scaler_url)
-
-    # Load the model and scaler from the response
-    model = joblib.load(io.BytesIO(model_response.content))
-    scaler = joblib.load(io.BytesIO(scaler_response.content))
 
     # Create input fields for user input
     inputs = {}
